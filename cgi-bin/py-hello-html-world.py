2885d79979eae2ecaa80ef3af314649d8b90a419
mrahdara <mrahdara@ucsd.edu>
2021-04-19 16:25:19 -0700

    py-hello-html-world.py

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,20 +1,20 @@
- #!/usr/bin/python
+#!/usr/bin/python
 
- import os
- import datetime 
- 
+import os
+import datetime
+import socket
 
-  // Print HTML header
-  print("Cache-Control: no-cache\n");
-  print("Content-type: text/html\n\n");
-  print("<html><head><title>Hello CGI World</title></head>\
-	<body><h1 align=center>Hello HTML World</h1>\
-  	<hr/>\n");
+print("Cache-Control: no-cache\n")
+print("Content-type: text/html\n\n")
+print("<html><head><title>Hello CGI World</title></head>\
+<body><h1 align=center>Hello HTML World</h1>\
+<hr/>\n")
 
 current_time = datetime.datetime.now
- print("Hello World<br/>\n");
- print("This program was generated at: {}\n<br/>").format(urrent_time);
- print("Your current IP address is: %s<br/>").format(os.environ["REMOTE_ADDR"]);
- 
- // Print HTML footer
- print("</body></html>");
+hostname = socket.gethostname()
+ip_address = ip_address = socket.gethostbyname(hostname)
+print("Hello World<br/>\n")
+print("This program was generated at: {}\n<br/>".format(current_time))
+print("Your current IP address is: %s<br/>".format(ip_address))
+
+print("</body></html>")

da08f89d33d71d3181ce53aff677da33396c7eaa
mrahdara <mrahdara@ucsd.edu>
2021-04-20 16:21:30 -0700

    py json

--- cgi-bin/py-hello-json-world.py
+++ cgi-bin/py-hello-json-world.py
@@ -2,17 +2,14 @@
 import cgi
 import os
 import datetime
-import socket
+import simplejson as json
 
 print("Cache-Control: no-cache")
 print("Content-type: application/json\r\n\r\n")
 
-time = datetime.datetime.now()
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
 
-j = {"message": "hello"}
+data = {"message": "hello"}
 
 #print(" Date & Time: {}".format(time))
 #print("Your current IP address is: {}<br/>".format(os.environ['REMOTE_ADDR']))
-print(j)
+print(json.JSONEncoder().encode(data))

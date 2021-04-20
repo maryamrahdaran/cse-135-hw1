ca254730e1a5414426816fa950e794f0003ee126
mrahdara <mrahdara@ucsd.edu>
2021-04-20 15:59:27 -0700

    py json

--- cgi-bin/py-hello-json-world.py
+++ cgi-bin/py-hello-json-world.py
@@ -11,9 +11,7 @@ time = datetime.datetime.now()
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-print("<html><head><title>Hello CGI World</title></head><body>")
-print("<h1 align=center>Hello HTML World</h1><hr/>")
-print("Hello World<br/>\n")
-print("This program was generated at: {}<br/>".format(time))
-print("Your current IP address is: {}<br/>".format(os.environ['REMOTE_ADDR']))
-print("</body></html>")
+
+print(" Date & Time: {}<br/>".format(time))
+#print("Your current IP address is: {}<br/>".format(os.environ['REMOTE_ADDR']))
+# print("</body></html>")

654ce5e39582a1f3eff8704f921af299f007c39a
mrahdara <mrahdara@ucsd.edu>
2021-04-20 13:46:04 -0700

    py-hello-html-world

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -3,15 +3,21 @@
 #import socket
 
 #print("Cache-Control: no-cache")
-print("Content-type: text/html\r\n\r\n")
-print("<html><head>")
-print("<title>Hello CGI World</title>")
-print("</head>")
-print("<body><h1 align=center>Hello HTML World</h1><hr/>\n")
+#print("Content-type: text/html\r\n\r\n")
+# print("<html><head>")
+#print("<title>Hello CGI World</title>")
+# print("</head>")
+#print("<body><h1 align=center>Hello HTML World</h1><hr/>\n")
 
 #current_time = datetime.datetime.now
 #hostname = socket.gethostname()
 #ip_address = ip_address = socket.gethostbyname(hostname)
-print("Hello World<br/>\n")
+#print("Hello World<br/>\n")
 #print("Your current IP address is: %s<br/>".format(ip_address))
+# print("</body></html>")
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\r\n\r\n")
+print("<html><body>")
+print("<h1> Hello Program! </h1>")
 print("</body></html>")

539d723a353ba6c6e66c844f77169ad5f50901e4
mrahdara <mrahdara@ucsd.edu>
2021-04-20 15:02:03 -0700

    py-hello-html-world

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-# Importing the 'cgi' module
 import cgi
+import os
+import datetime
+import socket
 
-
+print("Cache-Control: no-cache")
 print("Content-type: text/html\r\n\r\n")
-print("<html><body>")
-print("<h1> Hello Program! </h1>")
+
+time = datetime.datetime.now()
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+print("<html><head><title>Hello CGI World</title></head><body>")
+print("<h1 align=center>Hello HTML World</h1><hr/>")
+print("Hello World<br/>\n")
+print("This program was generated at: {}<br/>".format(time))
+print("Your current IP address is: {}<br/>".format(os.environ['REMOTE_ADDR']))
 print("</body></html>")

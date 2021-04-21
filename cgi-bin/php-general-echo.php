0fefb1adbab9e7ea3fb111991307ef4f3f30ea08
mrahdara <mrahdara@ucsd.edu>
2021-04-21 10:24:52 -0700

    php general echo

---
+++ cgi-bin/php-general-echo.php
@@ -0,0 +1,20 @@
+<?PHP
+  header('Cache-Control: no-cache');
+?>
+
+<html>
+<head>
+  <title>General Request Echo</title>
+</head>
+<body><h1 align="center">General Request Echo</h1>
+<hr>
+<?PHP
+$envs = $_SERVER;
+echo "<b>Protocol: </b>" .$envs['SERVER_PROTOCOL]. "<br/>";
+
+?>
+
+
+
+</body>
+</html>
--- cgi-bin/py-general-echo.py
+++ cgi-bin/py-general-echo.py
@@ -9,7 +9,6 @@ print("Content-type: text/html\r\n\r\n")
 
 print("<html><head><title>General Request Echo</title></head><body>")
 print("<h1 align=center>General Request Echo</h1><hr/>")
-print("Environment Variables<br/>\n")
 envs = os.environ
 print("<b>Protocol: </b> {} <br/>".format(envs['SERVER_PROTOCOL']))
 print("<b>Method: </b> {} <br/>".format(envs['REQUEST_METHOD']))

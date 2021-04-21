0b09beb51b5377ab0e1ade7ec647d949c61e23b6
mrahdara <mrahdara@ucsd.edu>
2021-04-20 19:20:25 -0700

    py post

---
+++ cgi-bin/py-post-echo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cgi
+import os
+import sys
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\r\n\r\n")
+
+
+print("<html><head><title>POST Request Echo</title></head><body>")
+print("<h1 align=center>POST Request Echo</h1><hr/>")
+print("<b>Message Body:</b></br>")
+data = sys.stdin.read()
+
+print("{}<br/>".format(data))
+#qlist = query.split('&')
+# for q in qlist:
+#eql = q.split('=')
+#print("{} = ".format(eql[0]))
+# if(len(eql) > 1):
+# print("{}<br/>".format(eql[1]))
+
+print("</body></html>")

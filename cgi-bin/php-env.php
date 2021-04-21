5fb7c324566cc2ed76255797f8d5901eb1379474
mrahdara <mrahdara@ucsd.edu>
2021-04-20 21:54:09 -0700

    php env

---
+++ cgi-bin/php-env.php
@@ -0,0 +1,23 @@
+<?PHP
+  header('Cache-Control: no-cache');
+?>
+<html>
+<head>
+<title>Hello, Php!</title>
+</head>
+<body>
+
+<p>This page was generated with the Php programming langauge</p>
+<?PHP
+
+
+$request = $_REQUEST
+
+
+echo "<p>Current Time: " .$request. "</p>";
+?>
+
+
+
+</body>
+</html>

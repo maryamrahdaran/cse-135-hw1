548bea40e759fb13608f1b6013ff266831cc2d5c
mrahdara <mrahdara@ucsd.edu>
2021-04-21 10:28:14 -0700

    php general echo

--- cgi-bin/php-general-echo.php
+++ cgi-bin/php-general-echo.php
@@ -8,13 +8,11 @@
 </head>
 <body><h1 align="center">General Request Echo</h1>
 <hr>
+
 <?PHP
 $envs = $_SERVER;
-echo "<b>Protocol: </b>" .$envs['SERVER_PROTOCOL]. "<br/>";
-
+//echo "<b>Protocol: </b>" .$envs['SERVER_PROTOCOL]. "<br/>";
 ?>
-
-
 
 </body>
 </html>

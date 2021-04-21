d55b34f1c3dba09b756ca2cb551d2a95ba9e3a09
mrahdara <mrahdara@ucsd.edu>
2021-04-21 09:12:36 -0700

    php post

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -9,8 +9,9 @@ print("Content-type: text/html\r\n\r\n")
 
 print("<html><head><title>POST Request Echo</title></head><body>")
 print("<h1 align=center>POST Request Echo</h1><hr/>")
-print("<b>Message Body:</b></br></br>")
+print("<b>Message Body: </b>")
 data = sys.stdin.read()
+print("{} </br></br>".format(data))
 dlist = data.split('&')
 for d in dlist:
     eql = d.split('=')

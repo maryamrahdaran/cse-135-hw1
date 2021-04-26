5a84cb370f4d826b7dcdd463be90692e297a2170
mrahdara <mrahdara@ucsd.edu>
2021-04-26 10:13:31 -0700

    py-session

--- cgi-bin/py-sessions-1.py
+++ cgi-bin/py-sessions-1.py
@@ -5,16 +5,14 @@ import datetime
 import socket
 
 print("Cache-Control: no-cache")
+print("Content-type: text/html\r\n\r\n")
 
   # Get Name from Environment
   name = sys.stdin.read()
 
    # Set the cookie using a header
    if len(name) > 0:
-        print("Content-type: text/html\n")
-        print("Set-Cookie: %s\n\n", name)
-    else:
-        print("Content-type: text/html\n\n")
+        print("Set-Cookie: {}\n\n".format(name))
 
     # Body - HTML
     print("<html>")
@@ -22,6 +20,10 @@ print("Cache-Control: no-cache")
     print("<body>")
     print("<h1>Python Sessions Page 1</h1>")
     print("<table>")
+    if os.environ.get("HTTP_COOKIE") != null:
+      print("<tr><td>Cookie:</td><td>%s</td></tr>\n", os.environ.get("HTTP_COOKIE"))
+    else:
+      print("<tr><td>Cookie:</td><td>None</td></tr>\n")
 
     print("</table>")
     print("</body>")

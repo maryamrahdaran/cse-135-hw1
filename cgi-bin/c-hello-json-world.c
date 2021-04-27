5a483e36a1a892653ee81b39fa094fd138266a63
mrahdara <mrahdara@ucsd.edu>
2021-04-26 21:13:23 -0700

    php session

--- cgi-bin/c-hello-json-world.c
+++ cgi-bin/c-hello-json-world.c
@@ -10,7 +10,8 @@ int main(void)
 	buffer[strlen(buffer) - 1] = '\0';
 	printf("Cache-Control: no-cache\r\n");
 	printf("Content-type: application/json\r\n\r\n");
-	printf("{\n\t\"message\": \"Hello World\",\n");
+	printf("{\n\t\"nmessage\": \"Hello World\",\n");
+	printf("{\n\t\"name\": \"Maryam Rahdara\",\n");
 	printf("\t\"date\": \"%s\",\n", buffer);
 	printf("\t\"currentIP\": \"%s\"\n}\n", getenv("REMOTE_ADDR"));
 	printf("\t\"name\": \"%s\"\n}\n", "maryam rahdaran");

f431538b3f96f8965827c2481b6e48ee601539e4
mrahdara <mrahdara@ucsd.edu>
2021-04-26 19:58:12 -0700

    php session

---
+++ cgi-bin/php-destroy-session.php
@@ -0,0 +1,20 @@
+<?PHP
+setcookie('username','destroyed');
+header('Cache-Control: no-cache');
+?>
+
+
+<html>
+<head><title>Python Session Destroyed</title></head>
+<body>
+<h1>Python Session Destroyed</h1>
+
+
+<a href=\"/cgi-bin/py-sessions-1.py\">Back to Page 1</a>
+<br />")
+<a href=\"/cgi-bin/py-sessions-2.py\">Back to Page 2</a>
+<br />")
+<a href=\"/py-form.html\">python Form</a>
+
+</body>
+</html>

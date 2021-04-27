cfab11c9236c8db06f64c3366b2c8c358502c3da
mrahdara <mrahdara@ucsd.edu>
2021-04-26 20:00:27 -0700

    php session

--- cgi-bin/php-destroy-session.php
+++ cgi-bin/php-destroy-session.php
@@ -10,11 +10,11 @@ header('Cache-Control: no-cache');
 <h1>Python Session Destroyed</h1>
 
 
-<a href=\"/cgi-bin/php-sessions-1.php\">Back to Page 1</a>
+<a href="/cgi-bin/php-sessions-1.php">Back to Page 1</a>
 <br />")
-<a href=\"/cgi-bin/php-sessions-2.php\">Back to Page 2</a>
+<a href="/cgi-bin/php-sessions-2.php">Back to Page 2</a>
 <br />")
-<a href=\"/py-form.html\">python Form</a>
+<a href="/php-form.html">python Form</a>
 
 </body>
 </html>
